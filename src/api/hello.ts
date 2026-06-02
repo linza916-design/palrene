a2f600b8cbaad8676cdd7973b51e0c4fd386aeb5
kyagabaalex <linza916@gmail.com>
2026-06-02 04:13:23 +0300

    HELLO IMPROVED

--- src/api/hello.ts
+++ src/api/hello.ts
@@ -1,5 +1,5 @@
 export default function handler(req: any, res: any) {
-  res.status(200).json({
-    message: "Hello from Vercel API",
+  return res.status(200).json({
+    message: "Vercel API works",
   });
 }
